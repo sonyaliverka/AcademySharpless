2fe314dbf2afb37b92abc61afbd7b16e72205eef
sonyaliverka <sonyaliverka@gmail.com>
2022-12-08 17:59:52 +0300

    add first task

---
+++ CSharpLinq/Program.cs
@@ -0,0 +1,21 @@
+﻿using static System.Formats.Asn1.AsnWriter;
+
+#region {Task_1}
+double[] nums = { 3, 15, 8, 43, 2, 9, 10, 72, 4, 1};
+
+IEnumerable<double> result =
+    from num in nums
+    where num   %3==0
+    orderby num
+    select num;
+
+foreach (double number in result)
+{
+    Console.Write(number + " ");
+}
+#endregion
+
+#region {Task_2}
+double[] num = { -7, 15, 8, -11, 7, -9, 10, -13, -10, 14 };
+
+#endregion
